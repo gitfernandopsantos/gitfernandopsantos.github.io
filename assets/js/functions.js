39d81cb9d17dda0ded46ef948195c0a3c1655ee9
Fernando Santos <fernandopsilvadossantos@gmail.com>
2024-09-28 21:32:14 -0300

    subindo a versão atualizada

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,3 +8,58 @@ function downloadCurriculo() {
     link.click();
     document.body.removeChild(link);
 };
+
+function openModal(title, body, imageUrls) {
+    document.getElementById('modalTitle').innerText = title;
+    document.getElementById('modalDescription').innerText = body;
+
+
+    const imageList = document.getElementById('imageList');
+    imageList.innerHTML = '';
+
+    imageUrls.forEach(url => {
+        const imgContainer = document.createElement('div');
+        imgContainer.className = 'me-2 mb-2';
+
+        const img = document.createElement('img');
+        img.src = url;
+        img.alt = "Imagem do projeto";
+        img.className = 'img-fluid';
+        img.style.maxHeight = '150px';
+        img.style.width = 'auto';
+
+        imgContainer.appendChild(img);
+        imageList.appendChild(imgContainer);
+    });
+
+    var modal = new bootstrap.Modal(document.getElementById('modalProjeto'));
+    modal.show();
+}
+
+
+function removeDuplicateBackdrops() {
+    const backdrops = document.querySelectorAll('.modal-backdrop');
+    if (backdrops.length > 1) {
+        for (let i = 1; i < backdrops.length; i++) {
+            backdrops[i].remove();
+        }
+    }
+}
+
+function closeModal() {
+    var modal = bootstrap.Modal.getInstance(document.getElementById('modalProjeto'));
+    if (modal) {
+        modal.hide();
+        setTimeout(() => {
+            document.body.classList.remove('modal-open');
+            document.body.style.overflow = '';
+        }, 300);
+    }
+
+    var backdrops = document.querySelectorAll('.modal-backdrop');
+    backdrops.forEach(function (backdrop) {
+        backdrop.remove();
+    });
+}
+
+
